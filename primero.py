854514978e28eb0fa9d9ad20810de2c3884bf9d2
tavooca <tavooca@gamil.com>
2019-03-03 23:07:47 +0100

    otro primero 4

--- primero.py
+++ primero.py
@@ -14,3 +14,11 @@ def max (n1, n2):
 // Un nuevo mensaje
 // --otro mensaje
 // 
+def min (n3, n4):
+    if n3 > n4:
+        print n3
+    elif n4 > n3:
+        print n4
+    else:
+        print "Son diferentes"
+        print n3
